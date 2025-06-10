d9a043f72532a263caff7c797323b287266c79a9
Lin Zhao <linzhao@uri.edu>
2025-06-10 21:15:43 +0000

    tested power monitor

---
+++ wamv_rise_bringup/launch/bringup_pi_test.launch.py
@@ -0,0 +1,62 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+
+def generate_launch_description():
+
+    robot_name = 'wamv_rise'
+
+    # Foxglove
+    foxglove = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('wamv_rise_bringup'),
+                'launch/include/foxglove_bridge.launch.xml')),
+        launch_arguments={
+            'namespace': robot_name
+        }.items()
+    )
+
+    # GPIO manager
+    gpio = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory('wamv_rise_bringup'), 
+            'launch/include/gpio_manager.launch.py')]),
+        launch_arguments={
+            'robot_name': robot_name,
+            'gpio_delay': '3.0'
+        }.items()  
+    )    
+
+    # Power Monitor
+    power = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory('wamv_rise_bringup'), 
+            'launch/include/power_monitor.launch.py')]),
+        launch_arguments={
+            'robot_name': robot_name,
+            'power_delay': '0.0'
+        }.items()  
+    ) 
+
+    # Comuter Monitor
+    computer = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory('wamv_rise_bringup'), 
+            'launch/include/computer_monitoring.launch.py')]),
+        launch_arguments={
+            'robot_name': robot_name,
+            'computer_delay': '0.0'
+        }.items()  
+    ) 
+
+
+    return LaunchDescription([
+        # foxglove,
+        # gpio,
+        power,
+        computer,
+    ])    
