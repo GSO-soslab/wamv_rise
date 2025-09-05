727bd16faebbb92070b8b59231f72b3926430531
Lin Zhao <linzhao@uri.edu>
2025-09-05 17:12:05 -0400

    update for c2 topside

--- wamv_rise_bringup/launch/include/mvp_c2_topside.launch.py
+++ wamv_rise_bringup/launch/include/mvp_c2_topside.launch.py
@@ -53,18 +53,18 @@ def generate_launch_description():
             parameters=[topside_setting_file],
         ),
 
-        # Node(
-        #     package="joy",
-        #     executable="joy_node",
-        #     name="joy_node",
-        #     namespace=robot_name,
-        #     output="screen",
-        #     parameters=[
-        #         {'coalesce_interval': 10},
-        #         {'autorepeat_rate': 0.0}
-        #     ],
-        #     remappings=[
-        #         ('joy', 'remote/id_2/joy'),
-        #     ]   
-        # ),
+        Node(
+            package="joy",
+            executable="joy_node",
+            name="joy_node",
+            namespace=robot_name,
+            output="screen",
+            parameters=[
+                # {'coalesce_interval': 10},
+                {'autorepeat_rate': 5.0}
+            ],
+            remappings=[
+                ('joy', 'remote/id_2/joy'),
+            ]   
+        ),
     ])
