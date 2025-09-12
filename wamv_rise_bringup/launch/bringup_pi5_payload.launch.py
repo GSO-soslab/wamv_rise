98d06fe964ce2dfe8206dfd6b5dc867366b463e4
Lin Zhao <linzhao@uri.edu>
2025-09-12 15:02:58 +0000

    update pi5 launch

--- wamv_rise_bringup/launch/bringup_pi5_payload.launch.py
+++ wamv_rise_bringup/launch/bringup_pi5_payload.launch.py
@@ -3,6 +3,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import ExecuteProcess
 
 def generate_launch_description():
 
@@ -51,5 +52,5 @@ def generate_launch_description():
         zenoh,
         livox,
         velodyne,
-        # norbit,
+        norbit,
     ])
